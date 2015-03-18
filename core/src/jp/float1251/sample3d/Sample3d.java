c11c8ff557350b9457fb60d96e15539936c5725a
takahiro.iwatani <taka.05022002@gmail.com>
2015-03-18 22:09:44 +0900

    Initial commit

---
+++ core/src/jp/float1251/sample3d/Sample3d.java
@@ -0,0 +1,63 @@
+package jp.float1251.sample3d;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.VertexAttributes;
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+
+public class Sample3d extends ApplicationAdapter {
+    ModelBatch batch;
+    private PerspectiveCamera camera;
+    private Model model;
+    private ModelInstance instance;
+    private Environment environment;
+    private CameraInputController cameraController;
+
+    @Override
+    public void create() {
+        batch = new ModelBatch();
+
+        environment = new Environment();
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, .4f, .4f, .4f, 1f));
+        environment.add(new DirectionalLight().set(.8f, .8f, .8f, -1f, -0.8f, -0.2f));
+
+        camera = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera.position.set(10, 10, 10);
+        camera.lookAt(0, 0, 0);
+        camera.near = 1f;
+        camera.far = 200f;
+        camera.update();
+
+        cameraController = new CameraInputController(camera);
+        Gdx.input.setInputProcessor(cameraController);
+
+        ModelBuilder builder = new ModelBuilder();
+        model = builder.createBox(5f, 5f, 5f,
+                new Material(ColorAttribute.createDiffuse(Color.CYAN)),
+                VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
+        instance = new ModelInstance(model);
+
+
+    }
+
+    @Override
+    public void render() {
+        cameraController.update();
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+        batch.begin(camera);
+        batch.render(instance, environment);
+        batch.end();
+    }
+}
---
+++ desktop/src/jp/float1251/sample3d/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package jp.float1251.sample3d.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import jp.float1251.sample3d.Sample3d;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Sample3d(), config);
+	}
+}
